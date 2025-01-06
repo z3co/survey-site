97a659c8138c0905556836851bea083eea967add
Jeppe Wolff Johansen <Jepper123411@proton.me>
2025-01-06 20:04:10 +0100

    Added views for auth routes and created middleware for user auth

--- index.js
+++ index.js
@@ -7,21 +7,27 @@ const multer = require("multer");
 const app = express();
 const cookieParser = require("cookie-parser");
 app.set("view engine", "ejs");
+const { userAuth } = require("./middleware/auth");
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "scripts")));
 const PORT = 3000;
 app.use("/api/routes", require("./auth/routes.js"));
 app.use("/api/surveys", require("./survey/routes.js"));
 
+app.get("/login", (req, res) => res.render("login"));
+app.get("/register", (req, res) => res.render("register"));
+app.get("/survey", userAuth, (req, res) => res.render("survey"));
+
 const Server = app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+	console.log(`Server listening on port ${PORT}`);
 });
 
 process.on("unhandledRejection", (err) => {
-  console.error("Unhandled rejection", err.message);
-  Server.close(() => {
-    process.exit(1);
-  });
+	console.error("Unhandled rejection", err.message);
+	Server.close(() => {
+		process.exit(1);
+	});
 });
 
 module.exports = app;
---
+++ middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+const jwtSecret = process.env.JWTSECRET;
+
+exports.userAuth = (req, res, next) => {
+	const token = req.cookies.jwt;
+	if (token) {
+		jwt.verify(token, jwtSecret, (err, decodedToken) => {
+			if (err) {
+				return res.status(401).json({ message: "Not authorized" });
+				// biome-ignore lint/style/noUselessElse: <explanation>
+			} else {
+				next();
+			}
+		});
+	} else {
+		return res.status(401).json({ message: "Not authorized" });
+	}
+};
