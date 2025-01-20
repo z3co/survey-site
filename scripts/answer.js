d6d005851a15de7a121235b89b192ed005e852b7
Jeppe Wolff Johansen <Jepper123411@proton.me>
2025-01-20 16:30:18 +0100

    Finished the logic and views for answering surveys

--- scripts/answer.js
+++ scripts/answer.js
@@ -3,17 +3,17 @@ const question = document.querySelector(".question");
 const answer = document.querySelector("#answer");
 const form = document.querySelector("form");
 const surveyId = Number(getUrlParams("survey"));
+const redirect = document.querySelector("#new");
 const number = document.querySelector(".number");
 
-const survey = (async () => {
-  return getQuestion(surveyId)
-})
+(async () => {
+	const survey = await getQuestion(surveyId);
+	question.textContent = survey.questions[number.textContent - 1].question;
+})();
 
 if (number.textContent === "") {
 	number.textContent = 1;
 }
-
-question.textContent = survey.questions[number.textContent - 1].question;
 
 async function getQuestion(surveyId) {
 	try {
@@ -27,6 +27,7 @@ async function getQuestion(surveyId) {
 		});
 		const data = await res.json();
 		if (res.status === 400 || res.status === 401) {
+			display.style.background = "red";
 			return (display.textContent = `${data.message}. ${data.error ? data.error : ""}`);
 		}
 		return data.survey;
@@ -47,7 +48,26 @@ function getUrlParams(target) {
 	return "NaN";
 }
 
+redirect.addEventListener("click", async (e) => {
+	location.assign("/surveyname");
+});
+
 form.addEventListener("submit", async (e) => {
 	e.preventDefault();
 	display.textcontent = "";
+	const survey = await getQuestion(surveyId);
+	if (survey.questions[number.textContent - 1].answer === answer.value) {
+		display.style.background = "green";
+		display.textContent = "Thats correct. Great job.";
+		number.textContent = Number(number.textContent) + 1;
+	} else {
+		display.style.background = "red";
+		display.textContent = "That is wrong, try again";
+	}
+	if (number.textContent > survey.questions.length) {
+		display.textContent = "You finished the survey.";
+		form.style.visibility = "hidden";
+		redirect.style.visibility = "visible";
+		sessionStorage.setItem("created-survey", false);
+	}
 });
